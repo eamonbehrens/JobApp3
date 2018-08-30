b7763f760ed91bcfc39771fad0096e8c5be8b49f
Eamon Behrens <ebehrens@valorem.com>
2018-08-30 10:36:14 -0500

    Authorization done

--- JobApp.API/DTO/UserForRegisterDto.cs
+++ JobApp.API/DTO/UserForRegisterDto.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace JobApp.API.DTO
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username {get; set;}
+
+        [Required]
+        [EmailAddress]
+        [StringLength(8, MinimumLength = 4, ErrorMessage= "Must be between 4-8 characters")]
         public string Password {get; set;}
         
     }
