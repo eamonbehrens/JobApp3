049f1fbbc67bba2d2c90db699e6ffef72bc5ab65
Eamon Behrens <ebehrens@valorem.com>
2018-09-19 14:44:10 -0500

    End of module 6

--- JobApp-SPA/src/app/app.module.ts
+++ JobApp-SPA/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import { BsDropdownModule } from 'ngx-bootstrap';
+
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import {FormsModule} from '@angular/forms';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { AlertifyService } from './_services/alertify.service';
 
 @NgModule({
    declarations: [
@@ -18,10 +23,13 @@ import { RegisterComponent } from './register/register.component';
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
-      AuthService
+      AuthService,
+      ErrorInterceptorProvider,
+      AlertifyService
    ],
    bootstrap: [
       AppComponent
--- JobApp-SPA/src/app/register/register.component.ts
+++ JobApp-SPA/src/app/register/register.component.ts
@@ -1,17 +1,16 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
-
+import { AlertifyService } from '../_services/alertify.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  @Input() valuesFromHome: any;
   @Output() cancelRegister = new  EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -19,14 +18,13 @@ export class RegisterComponent implements OnInit {
 this.authService.register(this.model).subscribe(() => {
   console.log('registration successful');
 }, error => {
-  console.log(error);
+  this.alertify.error(error);
 });
 
   }
 
   cancel() {
    this.cancelRegister.emit(false);
-    console.log('cancelled');
   }
 
 }
